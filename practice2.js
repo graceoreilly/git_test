e46f6a40eb8369954880bb2dbb7defa520b1054c
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-02 23:03:01 +0000

    Practice examples of objects and arrays

---
+++ practice2.js
@@ -0,0 +1,34 @@
+// Objects practice
+// https://www.youtube.com/watch?v=rLPwCAqyCAE
+
+// ------------------------------------------------------
+// const myObj = {
+//     name: "Grace"
+// };
+
+// const anotherObj = {
+//     alive: true,
+//     answer: 42,
+//    hobbies: ["Eat", "Sleep", "Code", "Repeat"], // This is an array
+//     beverage: { //this is another object
+//         morning: "coffee",
+//         evening: "wine"
+//     },
+//         action: function() { //this is a function within the object
+//             return `Time for ${this.beverage.evening}`;
+//         }
+//     };
+// ----------------------------------------------------------------------------------
+
+// // const user = {
+// //     age: 13,
+// //     name: "Stephen"
+// // }
+
+// // user.age = 45; //the age value can be reassigned even though the variable of the object is const
+
+// -----------------------------------------------------------------------------------
+
+// Arays
+// let places = ["cromane", "killorglin", "killarney"];
+// alert(places.at(-1)); //alerts killarney, the last value
